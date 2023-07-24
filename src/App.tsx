b891d0139f8dac7c4f332c2e83537e1e8940c333
batunpc <batuhnpc@gmail.com>
2023-07-23 23:57:21 -0400

    feat: support spline engine

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Layout from './components/Layout';
+// import {BrowserRouter, Route, Routes} from 'react-router-dom';
+// import Home from './pages/Home';
+// import NotFound from './pages/NotFound';
+// import Layout from './components/Layout';
 
-export function App() {
-    return (
-        <div className="bg-primary flex flex-col min-h-screen">
-            <Routes>
-                <Route element={<Layout/>}>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="*" element={<NotFound/>}/>
-                </Route>
-            </Routes>
-        </div>
-    );
-}
+// export function App() {
+//     return (
+//         <div className="bg-primary flex flex-col min-h-screen">
+//             <Routes>
+//                 <Route element={<Layout/>}>
+//                     <Route path="/" element={<Home/>}/>
+//                     <Route path="*" element={<NotFound/>}/>
+//                 </Route>
+//             </Routes>
+//         </div>
+//     );
+// }
 
-export function WrappedApp() {
-    return (
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    );
+// export function WrappedApp() {
+//     return (
+//         <BrowserRouter>
+//             <App/>
+//         </BrowserRouter>
+//     );
+// }
+
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/VVQYSJrKuTwNEfBj/scene.splinecode" />
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'tailwindcss/tailwind.css';
-import {WrappedApp} from './App';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <WrappedApp/>
-    </React.StrictMode>,
+import App from './App';
+import './styles.css';
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
